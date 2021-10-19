0978128763feef9e5771e06c05cb4c594d8bb272
Ulises Santana <ulises.santana@leanmind.es>
2021-10-19 19:30:41 +0100

    📝 update README and basic todos

---
+++ src/filterNumbersRepeatedOddTimes.test.ts
@@ -0,0 +1,6 @@
+describe('filterNumbersRepeatedOddTimes should', () => {
+  it.todo('return an array')
+  it.todo('return [2] for [2]')
+  it.todo('return [2] for [2,1,1]')
+  it.todo('return [2,1] for [2,1,1,1]')
+})
